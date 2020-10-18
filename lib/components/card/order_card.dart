421ed3350a9e3b178e9dc50d559338fd9bf22b95
admin <kalrobbynson@gmail.com>
2020-10-18 11:44:15 +0300

    order kist

--- lib/components/card/order_card.dart
+++ lib/components/card/order_card.dart
@@ -2,6 +2,7 @@ import 'package:cloud_9_client/models/order.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+
 typedef OrderCardOnTap = Function();
 typedef OrderMoreOnTap = Function();
 
@@ -18,6 +19,7 @@ class OrderCard extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Material(
@@ -30,10 +32,20 @@ class OrderCard extends StatelessWidget {
             ),
             onTap: orderListCardOnTap,
             title: Text(
-              order.buyerName,
+              'Product Name',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            subtitle: Text(order.result),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('No of Items:\t' + order.noOfItems),
+                Text(
+                  order.paymentStatus == "null"
+                      ? "Not Paid"
+                      : order.paymentStatus,
+                ),
+              ],
+            ),
             trailing: IconButton(
                 icon: Icon(Icons.more_vert), onPressed: orderMoreOnTap),
           )),
--- lib/screens/drawer_sreen.dart
+++ lib/screens/drawer_sreen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_9_client/api/api.dart';
 import 'package:cloud_9_client/constants/constants.dart';
 import 'package:cloud_9_client/provider/auth_provider.dart';
 import 'package:cloud_9_client/screens/account_screen.dart';
-import 'package:cloud_9_client/screens/help_screen.dart';
 import 'package:cloud_9_client/screens/order_screen.dart';
 import 'package:cloud_9_client/screens/terms_and_condition.dart';
 import 'package:flutter/material.dart';
